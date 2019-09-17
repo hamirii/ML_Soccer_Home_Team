9aa976be945ef340e1f4d9c8e4508bb49cb9eca7
hamiri <haris.amiri@live.com>
2019-09-16 20:49:03 -0700

    Commit

--- HomeGameAdvantage_Soccer.py
+++ HomeGameAdvantage_Soccer.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf 
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import classification_report
 
 
 
@@ -35,7 +36,7 @@ print(winRecords(pd.read_csv("epl_season-1819.csv"), 'EPL_18_19'))
 
 # Let's start with the training/learning/evaluating/testing Machine Learning Method! Beginning with Serie A 2018/19 dataset
 
-dataSerieA1819 = pd.read_csv("serieA_season-1516.csv")
+dataSerieA1819 = pd.read_csv("serieA_season-1819.csv")
 dataSerieA1819 = dataSerieA1819.drop('Div', axis=1)
 dataSerieA1819 = dataSerieA1819.drop('Date', axis=1)
 
@@ -62,15 +63,15 @@ X_train, X_test, y_train, y_test = train_test_split(x_data, y_labels, test_size=
 
 HomeTeam = tf.feature_column.categorical_column_with_hash_bucket('HomeTeam', hash_bucket_size=1000)
 AwayTeam = tf.feature_column.categorical_column_with_hash_bucket('AwayTeam', hash_bucket_size=1000)
+HTR  = tf.feature_column.sequence_categorical_column_with_hash_bucket('HTR', hash_bucket_size=1000)  # Half Time Result
 
 # Create tf.feature_columns for Numerical/Continuous Values
 
 FTHG = tf.feature_column.numeric_column('FTHG') # Full Time Home Goals Scored
 FTAG = tf.feature_column.numeric_column('FTAG') # Full Time Away Goals Scored
-FTR  = tf.feature_column.numeric_column('FTR')  # Full Time Result
+#FTR  = tf.feature_column.numeric_column('FTR')  # Full Time Result
 HTHG = tf.feature_column.numeric_column('HTHG') # Half Time Home Goals Scored
 HTAG = tf.feature_column.numeric_column('HTAG') # Half Time Away Goals Scored
-HTR  = tf.feature_column.numeric_column('HTR')  # Half Time Result
 HS   = tf.feature_column.numeric_column('HS')   # Home Shots
 AS   = tf.feature_column.numeric_column('AS')   # Away Shots
 HST  = tf.feature_column.numeric_column('HST')  # Home Shots on Target
@@ -86,7 +87,32 @@ AR  = tf.feature_column.numeric_column('AR')    # Away Red Cards
 
 
 # Compile into one features column
+#feature_cols = [HomeTeam,AwayTeam,FTHG,FTAG,FTR,HTHG,HTAG,HTR,HS,AS,HST,AST,HF,AF,HC,AC,HY,AY,HR,AR]
+feature_cols = [HomeTeam,AwayTeam,FTHG,FTAG,HTHG,HTAG,HTR,HS,AS,HST,AST,HF,AF,HC,AC,HY,AY,HR,AR]
 
-feature_cols = [HomeTeam,AwayTeam,FTHG,FTAG,FTR,HTHG,HTAG,HTR,HS,AS,HST,AST,HF,AF,HC,AC,HY,AY,HR,AR]
+# Building an input function
 
+input_fnc = tf.estimator.inputs.pandas_input_fn(x=X_train, y=y_train, batch_size=100, num_epochs=None, shuffle=True)
 
+# Using Linear Classifier to create a model with tf.Estimator
+
+model = tf.estimator.LinearClassifier(feature_columns=feature_cols)
+
+model.train(input_fn=input_fnc, steps=5000)
+
+# Now to make 'predictions'
+
+pred_fn = tf.estimator.inputs.pandas_input_fn(x=X_test, batch_size = len(X_test), shuffle=False)
+predictions = list(model.predict(input_fn=input_fnc))
+
+print(predictions[0])
+
+final_preds= []
+for pred in predictions:
+    final_preds.append(pred['class_ids'][0])
+
+print(final_preds[:10])
+
+# Import Classification report from SkLearn-Metrics to evaluate the performance of the test model
+
+print(classification_report(y_test, final_preds))
